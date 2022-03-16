9d46696e161ebc20d42b99e31453474f279173c6
Miles Kwok <83229800+KMGwei@users.noreply.github.com>
2022-03-16 12:21:28 +0800

    Navbar styling completed

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Header.scss';
+import { motion } from 'framer-motion';
+import { images } from '../../constants';
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <div>
+
+        </div>
     )
 }
 
